6a067ad894bdee970755639d90b1d512f5a44264
Zeus-HackOlympus <vishaljuneja707@gmail.com>
2021-05-05 05:18:07 +0530

    2.6.3 added

---
+++ module2/2.6.3-Escape-Characters.java
@@ -0,0 +1,16 @@
+/*
+ * Programmer: HackOlympus 
+ * Date: 5th May
+ * Purpose: Demonstrate the use of escape sequence 
+ */ 
+
+public class EscapeSequence{
+	public static void main(String[] args){
+		String name1 = "HackOlympus";
+		String name2 = "DeltaPsiFi";
+
+		System.out.println("\""+name1+"\"");
+		System.out.println("\\"+name2+"\\");
+		System.out.println("New\nLine Escape Sequence");
+	}
+}
