63625a5320fcd391235fc452fcd1d790cc2d8e1d
Zeus-HackOlympus <vishaljuneja707@gmail.com>
2021-05-05 11:04:28 +0530

    added 2.7.1

---
+++ module2/2.7.1-String-Methods.java
@@ -0,0 +1,24 @@
+/*
+ * Programmer: HackOlympus 
+ * Date: 5th May 
+ * Purpose: Demonstrate different String methods 
+ */ 
+
+// we will import a new package where string class is stored. 
+import java.lang.String; // semicolon required
+
+// we can also do 
+
+import java.lang.*; 
+
+
+public class StringMethods{
+	public static void main(String[] args){
+		String word = "computational" ; 
+		System.out.println(word.indexOf("n")); // prints 10 
+		System.out.println(word.indexOf("a")); 
+		System.out.println(word.indexOf("c"));
+	      	// This will print the index of very first 't' that java interpreter sees i.e 5  	
+		System.out.println(word.indexOf("t")); 
+	}
+}
