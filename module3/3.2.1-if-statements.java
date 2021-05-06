b4de63e54242aa8556f27a7e0b94f3f600c59b8c
Zeus-HackOlympus <vishaljuneja707@gmail.com>
2021-05-06 06:03:58 +0530

    Module 3

---
+++ module3/3.2.1-if-statements.java
@@ -0,0 +1,17 @@
+/*
+ * Programmer: HackOlympus 
+ * Date: 5th May 
+ * Purpose: Demonstrate how if statements work 
+ */ 
+
+public class ifStatements
+{ 
+	public static void main(String[] args)
+	{
+		int num = 16 ; 
+		if (num > 0){
+			System.out.println("Number is +ve") ;
+		}
+	}
+}
+
---
+++ module3/3.2.2-if-else-statements.java
@@ -0,0 +1,21 @@
+/*
+ * Programmer: HackOlympus
+ * Date: 5th May 
+ * Purpose: Demonstrate hoe if-else statements work 
+ */ 
+
+public class if_else_statements
+{
+	public static void main(String[] args)
+	{
+		//int num = 16 ; 
+		// int num = -1 ; 
+		int num = 0 ; 
+		if (num > 0){
+			System.out.println("Number is positive"); 
+		}
+		else{
+			System.out.println("Number is either -ve or < 0 ") ; 
+		}
+	}
+}
