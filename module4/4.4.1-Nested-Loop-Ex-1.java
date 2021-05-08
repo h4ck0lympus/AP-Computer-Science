e1f5bb66949655cb252fd82bd68cb4198276ab14
Zeus-HackOlympus <vishaljuneja707@gmail.com>
2021-05-08 14:06:38 +0530

    4.4.1 added

---
+++ module4/4.4.1-Nested-Loop-Ex-1.java
@@ -0,0 +1,17 @@
+/*
+ * Programmer: HackOlympus 
+ * Date: 8th May 
+ * Purpose: Nested itration example 1 
+ */
+
+public class NestedIteration1 
+{
+	public static void main(String[] args)
+	{
+		for (int i = 0 ; i <= 23 ; i++){
+			for (int j = 0 ; j <= 59 ; j++){
+				System.out.println(i+":"+j); 
+			}
+		}
+	}
+}
